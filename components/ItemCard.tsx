687cd75ff7a09b6cc8bfe545b783647eec92835b
Chibuking35 <cnwigwe525@gmail.com>
2026-01-29 14:11:50 +0000

    file add

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -5,7 +5,7 @@ export default function ItemCard({ item }: any) {
   return (
     <Link href={`/items/${item.id}`}>
       <div className="bg-gray-100 rounded overflow-hidden cursor-pointer hover:shadow-lg transition">
-        <h3 className="font-semibold text-red-500 p-2">
+        <h3 className="font-semibold text-red-500 p-2 min-h-5">
           {item.title}
         </h3>
 
