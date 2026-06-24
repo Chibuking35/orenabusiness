2767a3749c4424ce5b6699c375ca81327f62e721
Chibuking35 <cnwigwe525@gmail.com>
2026-06-24 04:16:49 +0100

    s

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -56,63 +56,58 @@ const Slider = () => {
   }, []);
 
   return (
+    <div className="w-full  flex flex-col lg:flex-row">
  
-     <div className="md:w-2/2 relative items-center justify-center h-96 md:h-screen w-full flex flex-col lg:flex-row">
-  {/* Background Image */}
-  <Image
-    src={slides[current].image}
-    alt={slides[current].word}
-    fill
-    className="object-cover transition-opacity duration-1000 relative"
-  />
+     
 
-  <div className="bg-black/60 z-20 absolute inset-0" />
+      
+      <div className="md:w-2/2 relative flex items-center  justify-center h-96 md:h-screen">
+        {/* Background Image */}
+        <Image
+          src={slides[current].image}
+          alt={slides[current].word}
+          fill
+          className="object-cover transition-opacity duration-1000 relative"
+        />
+        <div className="bg-black/60 z-20 absolute inset-0" />
 
-  <div className="absolute inset-0 z-20 items-center flex flex-col">
-    {/* Center Content */}
-    <div className="flex-1 flex items-center px-5">
-      <div className="text-left">
-        <h1 className="text-2xl md:text-4xl text-white font-bold drop-shadow-lg">
-          {slides[current].word}
-        </h1>
+        {/* Word in front */}
+        <div className="absolute z-20 items-center px-5  justify-center">
+          <div className="text-left">
+            <h1 className="text-2xl md:text-4xl text-white font-bold drop-shadow-lg">
+              {slides[current].word}
+            </h1>
+            <p className="mt-2 text-gray-400 text-sm md:text-lg font-normal w-[70%] md:w-full">
+              {slides[current].description}
+            </p>
+          </div>
+        </div>
 
-        <p className="mt-2 text-gray-400 text-sm md:text-lg font-normal w-[70%] md:w-full">
-          {slides[current].description}
-        </p>
+        {/* Bottom Info Bar */}
+        <div className="absolute bottom-5 hidden md:flex opacity-60 w-[90%] bg-gray-300 backdrop-blur-2xl bg-opacity-50 z-20 text-gray-600 rounded-md  justify-between p-5">
+          <div className="flex-1 text-center flex-col flex">
+            <h1 className="font-bold">Quality</h1>
+            <p className="text-xs font-light">
+              Premium materials, clean finishes, and lasting prints
+            </p>{" "}
+          </div>
+          <div className="w-px relative mx-3 h-9 bg-gray-600" />
+          <div className="flex-1 text-center flex-col flex">
+            <h1 className="font-bold">Creativity</h1>
+            <p className="text-xs font-light">
+              Designs that tell stories and express identity
+            </p>{" "}
+          </div>
+          <div className="w-px relative mx-4 h-9 bg-gray-600" />
+          <div className="flex-1 text-center flex-col flex">
+            <h1 className="font-bold">Precision</h1>
+            <p className="text-xs font-light">
+              Attention to detail in every print and customization
+            </p>{" "}
+          </div>
+        </div>
       </div>
     </div>
-
-    {/* Bottom Info Bar */}
-    <div className="hidden md:flex absolute bottom-5 left-1/2 -translate-x-1/2 w-[90%] opacity-60 bg-gray-300 backdrop-blur-2xl bg-opacity-50 z-20 text-gray-600 rounded-md justify-between p-5">
-      <div className="flex-1 text-center flex-col flex">
-        <h1 className="font-bold">Quality</h1>
-        <p className="text-xs font-light">
-          Premium materials, clean finishes, and lasting prints
-        </p>
-      </div>
-
-      <div className="w-px relative mx-3 h-9 bg-gray-600" />
-
-      <div className="flex-1 text-center flex-col flex">
-        <h1 className="font-bold">Creativity</h1>
-        <p className="text-xs font-light">
-          Designs that tell stories and express identity
-        </p>
-      </div>
-
-      <div className="w-px relative mx-4 h-9 bg-gray-600" />
-
-      <div className="flex-1 text-center flex-col flex">
-        <h1 className="font-bold">Precision</h1>
-        <p className="text-xs font-light">
-          Attention to detail in every print and customization
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
-  
-    
   );
 };
 
